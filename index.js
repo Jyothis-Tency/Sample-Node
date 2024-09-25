0dc749fc3b882b6f7ccfcb18b824c5942fbac8d5
Jyothis-Tency <jyothisgtency@gmail.com>
2024-09-25 10:29:15 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const PORT = 8000;
+
+const app = express();
+
+app.set("view-engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.listen(PORT, () => {
+  console.log("Server started");
+});
